644486c95b181ab2260cfd438169853421cf462e
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-09-08 18:17:59 +0200

    fix unit tests running e2e

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
 import { getViteConfig } from 'astro/config';
+import { configDefaults, defineConfig } from 'vitest/config'
+
 
 export default getViteConfig({
   test: {
+    exclude: [
+      ...configDefaults.exclude,
+      'tests/e2e/*',
+    ]
     // Vitest configuration options
   },
 });
